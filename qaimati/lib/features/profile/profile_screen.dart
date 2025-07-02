50e51f8d94e4ae9c53750070495f4ba079717696
Shatha 01 <shtwaijri>
2025-07-02 12:20:08 +0300

    save some files like nav

--- qaimati/lib/features/profile/profile_screen.dart
+++ qaimati/lib/features/profile/profile_screen.dart
@@ -1,68 +1,93 @@
-import 'dart:core';
+// import 'dart:core';
 
-import 'package:flutter/material.dart';
-import 'package:qaimati/style/style_text.dart';
-import 'package:qaimati/utilities/extensions/screens/get_size_screen.dart';
-import 'package:qaimati/widgets/app_bar_widget.dart';
-import 'package:qaimati/features/profile/widgets/custom_widget_setting.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qaimati/style/style_color.dart';
+// import 'package:qaimati/style/style_text.dart';
+// import 'package:qaimati/utilities/extensions/screens/get_size_screen.dart';
+// import 'package:qaimati/widgets/app_bar_widget.dart';
+// import 'package:qaimati/features/profile/widgets/custom_widget_setting.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+// class ProfileScreen extends StatelessWidget {
+//   const ProfileScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(
-        title: 'Profile',
-        showActions: false,
-        showSearchBar: false,
-      ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBarWidget(
+//         title: 'Profile',
+//         showActions: false,
+//         showSearchBar: false,
+//       ),
 
-      body: Padding(
-        padding: EdgeInsetsGeometry.only(right: 20),
-        // padding: EdgeInsets.symmetric(
-        //   horizontal: context.getWidth() * 0.3,
-        //   vertical: context.getHeight() * 0.03,
-        // ),
-        child: Column(
-          children: [
-            Text("name", style: StyleText.bold16(context)),
-            Text("name@gmail.com"),
-            Text("Setting", style: StyleText.bold16(context)),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: EdgeInsets.symmetric(horizontal: context.getWidth() * 0.05),
 
-            CustomWidgetSetting(
-              icon: Icons.language,
-              text: 'Change app language',
-              style: StyleText.bold16(context),
-            ),
-            CustomWidgetSetting(
-              icon: Icons.color_lens,
-              text: 'Change app color',
-              style: StyleText.bold16(context),
-            ),
-            Text("Account", style: StyleText.bold16(context)),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
 
-            // Row(
-            //   children: [Icon(Icons.language), Text("name@Change app language")],
-            // ),
-            CustomWidgetSetting(
-              icon: Icons.person,
-              text: 'Change account name',
-              style: StyleText.bold16(context),
-            ),
-            CustomWidgetSetting(
-              icon: Icons.mail,
-              text: 'Change account email',
-              style: StyleText.bold12(context),
-            ),
-            CustomWidgetSetting(
-              icon: Icons.star,
-              text: 'Prime plus',
-              style: StyleText.bold16(context),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//             children: [
+//               Text("name", style: StyleText.bold16(context)),
+//               SizedBox(height: context.getHeight() * 0.03),
+//               Text("name@gmail.com", style: StyleText.bold16(context)),
+//               SizedBox(height: context.getHeight() * 0.03),
+//               Text("Setting", style: StyleText.bold16(context)),
+//               SizedBox(height: context.getHeight() * 0.03),
+
+//               CustomWidgetSetting(
+//                 icon: Icons.language,
+//                 text: 'Change app language',
+//                 style: StyleText.bold16(context),
+//                 iconColor: StyleColor.green,
+//                 iconSize: context.getWidth() * 0.06,
+//               ),
+//               SizedBox(height: context.getHeight() * 0.03),
+
+//               CustomWidgetSetting(
+//                 icon: Icons.color_lens,
+//                 text: 'Change app color',
+//                 style: StyleText.bold16(context),
+//                 iconColor: StyleColor.green,
+//                 iconSize: context.getWidth() * 0.06,
+//               ),
+//               SizedBox(height: context.getHeight() * 0.03),
+//               // Icon(Icons.abc, color: Colors.green),
+//               Text("Account", style: StyleText.bold16(context)),
+//               SizedBox(height: context.getHeight() * 0.03),
+
+//               // Row(
+//               //   children: [Icon(Icons.language), Text("name@Change app language")],
+//               // ),
+//               CustomWidgetSetting(
+//                 icon: Icons.person,
+//                 text: 'Change account name',
+//                 style: StyleText.bold16(context),
+//                 iconColor: StyleColor.green,
+//                 iconSize: context.getWidth() * 0.06,
+//               ),
+//               SizedBox(height: context.getHeight() * 0.03),
+
+//               CustomWidgetSetting(
+//                 icon: Icons.mail,
+//                 text: 'Change account email',
+//                 style: StyleText.bold16(context),
+//                 iconColor: StyleColor.green,
+//                 iconSize: context.getWidth() * 0.06,
+//               ),
+//               SizedBox(height: context.getHeight() * 0.03),
+
+//               CustomWidgetSetting(
+//                 icon: Icons.star,
+//                 text: 'Prime plus',
+//                 style: StyleText.bold16(context),
+//                 iconColor: StyleColor.green,
+//                 iconSize: context.getWidth() * 0.06,
+//               ),
+//               SizedBox(height: context.getHeight() * 0.03),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
