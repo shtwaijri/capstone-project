8bd697533d64e0b0672dd7a8bbc538ec6d7687c7
BushraAljuwair <bushraaljuwair@gmail.com>
2025-07-05 11:34:59 +0300

    add comments to sublist & complted events and states

--- qaimati/lib/features/sub_list/bloc/sub_list_state.dart
+++ qaimati/lib/features/sub_list/bloc/sub_list_state.dart
@@ -1,17 +1,23 @@
 // lib/bloc/sub_list/sub_list_state.dart
-
+/// Base class for all states emitted by the SubListBloc.
 part of 'sub_list_bloc.dart';
 
 @immutable
 sealed class SubListState {}
 
+/// Initial state of the SubListBloc, typically emitted right after creation.
+
 final class SubListInitial extends SubListState {}
 
+/// State indicating that the SubListBloc is currently performing a long-running
+/// operation,fetching data from a database.
 final class SubListLoading extends SubListState {}
 
+/// State representing the successful loading of data for the main SubList screen.
+/// It contains all the necessary data to render the UI.
 final class SubListLoadedState extends SubListState {
   final List<ItemModel> uncompletedItems;
- 
+
   final int currentNumber;
   final bool currentIsItemImportant;
   final bool isItemsChecked;
@@ -20,7 +26,7 @@ final class SubListLoadedState extends SubListState {
 
   SubListLoadedState({
     required this.uncompletedItems,
-     this.currentNumber = 1,
+    this.currentNumber = 1,
     this.currentIsItemImportant = false,
     this.isItemsChecked = false,
     this.currentUserRole,
@@ -28,21 +34,25 @@ final class SubListLoadedState extends SubListState {
   });
 }
 
+/// State representing the successful loading of completed items, grouped by list name.
 final class CompletedItemsLoadedState extends SubListState {
   final Map<String, List<ItemModel>> completedItemsByListName;
   CompletedItemsLoadedState({required this.completedItemsByListName});
 }
 
+/// State indicating that an error has occurred within the SubListBloc.
 final class SubListError extends SubListState {
   final String message;
   SubListError({required this.message});
 }
 
+///State indicating that an item deletion operation has been performed.
+/// This state  used to trigger a UI refresh or show a confirmation.
 final class DeleteItemState extends SubListState {}
 
+/// State indicating that a checkout process will start.
 final class CheckoutState extends SubListState {}
 
+/// State indicating that selected checked items have been successfully marked as completed.
 final class MarkCheckedItemsAsCompletedState extends SubListState {}
-
  
- final class LoadCompletedItemsForListsState  extends SubListState {}
--- qaimati/lib/features/sub_list/completed_screen/bloc/completed_screen_state.dart
+++ qaimati/lib/features/sub_list/completed_screen/bloc/completed_screen_state.dart
@@ -4,16 +4,22 @@ part of 'completed_screen_bloc.dart';
 @immutable
 sealed class CompletedScreenState {}
 
+/// The initial state of the CompletedScreenBloc.
+
 final class CompletedScreenInitial extends CompletedScreenState {}
 
+/// State indicating that the CompletedScreenBloc is currently loading data.
 class LoadingScreenState extends CompletedScreenState {}
 
+/// State indicating that an error has occurred during a process in the CompletedScreenBloc.
 class ErrorState extends CompletedScreenState {
   final String? message;
 
   ErrorState({required this.message});
 }
 
+/// State representing the successful retrieval and display of completed items data.
+/// The `completedItemsMap` contains the organized data to be shown on the screen.
 // ignore: must_be_immutable
 class GetDataScreenState extends CompletedScreenState {
   Map<String, List<ItemModel>> completedItemsMap;
